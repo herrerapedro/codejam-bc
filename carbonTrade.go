de0a9dee1a3a3beacafb7045493740d37330e308
Jarman <jley@ca.ibm.com>
2017-03-06 12:54:08 -0400

    added function main()

--- carbonTrade.go
+++ carbonTrade.go
@@ -102,3 +102,10 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 
 
 }
+
+func main() {
+	err := shim.Start(new(SimpleChaincode))
+	if err != nil {
+		fmt.Printf("Error starting Simple chaincode: %s", err)
+	}
+}
