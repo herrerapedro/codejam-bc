ea1904661eb99df257328c7ac4c0ca90603ea613
Jarman <jley@ca.ibm.com>
2017-03-06 15:05:29 -0400

    added create XOM/customer, trade between them

--- carbonTrade.go
+++ carbonTrade.go
@@ -41,7 +41,6 @@ type Carrier struct {
 
 type Customer struct {
   Name            string `json:"name"`
-  Delivered       string `json:"delivered"`
   CurrentBal      string `json:"currentBal"`
 }
 //==============================================================================================================================
@@ -50,10 +49,14 @@ type Customer struct {
 //==============================================================================================================================
 type XOM struct {
   Name            string `json:"name"`
-  Delivered       string `json:"delivered"`
   CurrentBal      string `json:"currentBal"`
 }
 
+
+//==============================================================================================================================
+//	Order - Defines the structure for a Order object. JSON on right tells it what JSON fields to map to
+//			  that element when reading a JSON object into the struct e.g. JSON owner -> Struct owner.
+//==============================================================================================================================
 type Order  struct{
   SerialNum   string `json:"serialNum"`
   AckCus      string `json:"ackCus"`     //customer acknowledgement
@@ -61,7 +64,6 @@ type Order  struct{
   AckCarrier  string `json:"ackCarrier"`//customer acknowledgement
   Cost        string `json:"cost"`
 }
-
 
 
 // SimpleChaincode example simple Chaincode implementation
@@ -77,6 +79,12 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
   switch function {
   case "createOrder":
     return t.createOrder(stub,args)
+  case "createCustomer":
+    return t.createCustomer(stub,args)
+  case "createXOM":
+    return t.createXOM(stub,args)
+  case "tradeFunds":
+    return t.tradeFunds(stub,args)
   }
 return nil,nil
 }
@@ -90,12 +98,120 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
   return nil,nil
 }
 
+//==============================================================================================================================
+//  Transfer funds
+//==============================================================================================================================
+func (t *SimpleChaincode) tradeFunds(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+A:=args[0]
+B:=args[1]
+amount:=[2]
+
+//get the balances
+var provider XOM
+valueA, err := stub.GetState(A)
+err=json.Unmarshal(valueA,&provider)
+CBX,err:=strconv.Atoi(provider.currentBal)
+
+var customer Customer
+valueB, err := stub.GetState(B)
+err=json.Unmarshal(valueB, &customer)
+CBC,err:=strconv.Atoi(customer.currentBal)
+
+//now the trade
+CBC=CBC-amount
+CBX=CBX+amount
+
+a1,_=json.Marshal(provider)
+err = stub.PutState(A,[]byte(a1))
+
+b1,_=json.Marshal(customer)
+err = stub.PutState(B,[]byte(a1))
+
+
+}
+//==============================================================================================================================
+//  XOM Creation
+//==============================================================================================================================
+func (t *SimpleChaincode) createXOM(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+  if len(args) != 2 {
+    return nil, errors.New("Incorrect number of arguments. Expecting 3")
+  }
+
+  X:=args[0]//serial number
+  valueC, err := stub.GetState(X)//get state of previous Order
+  var carbon XOM//create order object
+  err = json.Unmarshal(valueC, &carbon)
+
+  X2 := &XOM{
+    Name: args[0],
+    currentBal:args[1],
+  }
+
+  x1,_:=json.Marshal(X2)
+  serialnum:= args[0]
+
+  err = stub.PutState(serialnum, []byte(x1)) // passes serialNum as the key value for searching blockchain
+  if err != nil {
+    return nil, err
+
+  }
+  return nil,nil
+}
+//==============================================================================================================================
+//  Customer Creation
+//==============================================================================================================================
+func (t *SimpleChaincode) createCustomer(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+  if len(args) != 2 {
+    return nil, errors.New("Incorrect number of arguments. Expecting 3")
+  }
+
+  C:=args[0]//serial number
+  valueC, err := stub.GetState(C)//get state of previous Order
+  var customer Customer//create order object
+  err = json.Unmarshal(valueC, &customer)
+
+  C2 := &Customer{
+    Name: args[0],
+    currentBal:args[1],
+  }
+
+  c1,_:=json.Marshal(C2)
+  serialnum:= args[0]
+
+  err = stub.PutState(serialnum, []byte(c1)) // passes serialNum as the key value for searching blockchain
+  if err != nil {
+    return nil, err
+
+  }
+  return nil,nil
+}
+
+//==============================================================================================================================
+//  Read Customer
+//==============================================================================================================================
+func (t *SimpleChaincode) readCustomer(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+
+  if len(args) != 1 {
+      return nil, errors.New("Incorrect number of arguments. Expecting Serial Number")
+    }
+
+    value, err := stub.GetState(args[0]) //args 0 = serialNum
+    if err != nil {
+      return nil, errors.New("blank error msg")
+    }
+    return value,nil
+
+}
+
+//==============================================================================================================================
+//  Order Creation
+//==============================================================================================================================
 func (t *SimpleChaincode) createOrder(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
   if len(args) != 5 {
     return nil, errors.New("Incorrect number of arguments. Expecting 5")
   }
 
-  O:=args[0]//account ID
+  O:=args[0]//serial number
   valueO, err := stub.GetState(O)//get state of previous Order
   var order Order//create order object
   err = json.Unmarshal(valueO, &order)
@@ -114,14 +230,17 @@ func (t *SimpleChaincode) createOrder(stub shim.ChaincodeStubInterface, args []s
   err = stub.PutState(serialnum, []byte(o1)) // passes serialNum as the key value for searching blockchain
   if err != nil {
     return nil, err
-  
+
   }
   return nil,nil
 }
 
+//==============================================================================================================================
+//  Read Order
+//==============================================================================================================================
 func (t *SimpleChaincode) readOrder(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
   if len(args) != 1 {
-      return nil, errors.New("Incorrect number of arguments. Expecting AccountID")
+      return nil, errors.New("Incorrect number of arguments. Expecting Serial Number")
     }
 
     value, err := stub.GetState(args[0]) //args 0 = serialNum
